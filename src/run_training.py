833af98f0d4a124f40f979b72805bbb19c08df37
Maciej Zakrzewski <etavpl@gmail.com>
2022-09-07 21:12:02 +0200

    Add experiment type argument to argparser in run_training

--- src/run_training.py
+++ src/run_training.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
     parser.add_argument("gpu", type=int, help="GPU no")
     parser.add_argument("metadata", type=str, help="Metadata path")
     parser.add_argument("dataset", type=str, help="Dataset path")
+    parser.add_argument("experiment", type=str, help="Experimenal layer",
+                        choices=["normal_unet", "hard_tanh", "adaptive_sigmoid", "adaptive_tanh"])
     parser.add_argument("-o", action='store_true', help="Overwrite previous trainings dirs")
     args = parser.parse_args()
 
@@ -33,7 +35,6 @@ if __name__ == "__main__":
     device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
 
     # U-net
-
     training_config_normalunet = TrainingConfig()
     training_config_window_hard_tanh_unet = TrainingConfig(WindowLayerHardTanH())
     training_config_window_adaptive_sigmoid_unet = TrainingConfig(
@@ -43,38 +44,40 @@ if __name__ == "__main__":
         WindowLayerAdaptiveTanh()
     )
 
-    for config, name in [
-        (training_config_normalunet, "unet"),
-        # (training_config_window_hard_tanh_unet, "unet-hard-tanh-window"),
-        # (training_config_window_adaptive_sigmoid_unet, "unet-adaptive-sigmoid-window"),
-        # (training_config_window_adaptive_tanh_unet, "unet-adaptive-tanh-window"),
-    ]:
-        if args.o:
-            config.overwrite_previous_trainings = True
-            print("Overwriting previous trainings enabled")
+    experiments = {"normal_unet": (training_config_normalunet, "unet"),
+                   "hard_tanh": (training_config_window_hard_tanh_unet, "unet-hard-tanh-window"),
+                   "adaptive_sigmoid": (training_config_window_adaptive_sigmoid_unet, "unet-adaptive-sigmoid-window"),
+                   "adaptive_tanh": (training_config_window_adaptive_tanh_unet, "unet-adaptive-tanh-window")}
 
-        config.batch_size = args.batch_size
-        metadata = load_metadata(args.metadata)
-        print(metadata)
-        metadata.drop("series_id", axis=1, inplace=True)
-        metadata = metadata.to_numpy()
-        dataset = TomographyDataset(args.dataset, metadata)
-        config.epochs = args.epochs
+    config, name = experiments[args.experiment]
+    print(f"Running {args.experiment}")
 
-        train, test = dataset.train_test_split(0.2)
-        print(test)
-        folds = dataset.k_fold_split(train, k=training_config_normalunet.k_folds)
-        print(folds)
+    if args.o:
+        config.overwrite_previous_trainings = True
+        print("Overwriting previous trainings enabled")
 
-        folds_data_loaders = dataset.create_k_fold_data_loaders(
-            folds, batch_size=training_config_normalunet.batch_size
-        )
+    config.batch_size = args.batch_size
+    metadata = load_metadata(args.metadata)
+    print(metadata)
+    metadata.drop("series_id", axis=1, inplace=True)
+    metadata = metadata.to_numpy()
+    dataset = TomographyDataset(args.dataset, metadata)
+    config.epochs = args.epochs
 
-        for i, fold_data_loaders in enumerate(folds_data_loaders):
-            if i == args.gpu:
-                run_training(
-                    f"{name}-fold-{i}",
-                    training_config_normalunet,
-                    device,
-                    fold_data_loaders,
-                )
+    train, test = dataset.train_test_split(0.2)
+    print(test)
+    folds = dataset.k_fold_split(train, k=training_config_normalunet.k_folds)
+    print(folds)
+
+    folds_data_loaders = dataset.create_k_fold_data_loaders(
+        folds, batch_size=training_config_normalunet.batch_size
+    )
+
+    for i, fold_data_loaders in enumerate(folds_data_loaders):
+        if i == args.gpu:
+            run_training(
+                f"{name}-fold-{i}",
+                training_config_normalunet,
+                device,
+                fold_data_loaders,
+            )
