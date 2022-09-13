1283bc21c59e94f1f1b40997c565666a07cfece8
DominikKwiatkowski <domik.rex@gmail.com>
2022-09-13 19:37:57 +0200

    update train script.

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-nohup python ./src/run_training.py -o 8 50 1 0 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "normal_unet" 2>normal_unet.log 1>normal_unet.err &
-nohup python ./src/run_training.py -o 8 50 2 0 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "hard_tanh" 2>hard_tanh.log 1>hard_tanh.err &
-nohup python ./src/run_training.py -o 8 50 3 0 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_sigmoid" 2>adaptive_sigmoid.log 1>adaptive_sigmoid.err &
-nohup python ./src/run_training.py -o 8 50 4 0 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_tanh" 2>adaptive_tanh.log 1>adaptive_tanh.err &
+nohup python ./src/run_training.py -o 8 50 1 0 0.001 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "normal_unet001" 2>normal_unet001.log 1>normal_unet001.err &
+nohup python ./src/run_training.py -o 8 50 2 0 0.005 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "normal_unet005" 2>normal_unet005.log 1>normal_unet005.err &
+nohup python ./src/run_training.py -o 8 50 3 0 0.0001 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "normal_unet0001" 2>normal_unet0001.log 1>normal_unet0001.err &
+nohup python ./src/run_training.py -o 8 50 4 0 0.005 /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "normal_unet0005" 2>normal_unet0005.log 1>normal_unet0005.err &
