99c8081569701ed4b12af2a693ddfee345180280
Maciej Zakrzewski <etavpl@gmail.com>
2022-10-05 20:52:44 +0200

    Add experiment names

--- experiments/128_batch_liver_and_256_nobatch_tumor.sh
+++ experiments/128_batch_liver_and_256_nobatch_tumor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-python ./src/run_training.py 64 50 1 0 0.00005 0.00005 128 --use_batch_norm /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_sigmoid" 2>logs/128_batch_liver.log 1>logs/128_batch_liver.err &
-python ./src/run_training.py 64 50 1 0 0.00005 0.00005 256 --tumor  /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_sigmoid" 2>logs/256_nobatch_tumor.log 1>logs/256_nobatch_tumor.err &
+python ./src/run_training.py 64 50 1 0 0.00005 0.00005 128 --use_batch_norm /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_sigmoid" "128_batch_liver" 2>logs/128_batch_liver.log 1>logs/128_batch_liver.err &
+python ./src/run_training.py 64 50 1 0 0.00005 0.00005 256 --tumor  /home/macierz/s175573/tomography/lits_prepared/metadata.csv /home/macierz/s175573/tomography/lits_prepared "adaptive_sigmoid" "256_nobatch_tumor" 2>logs/256_nobatch_tumor.log 1>logs/256_nobatch_tumor.err &
 
